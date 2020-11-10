aaf597e926ed5d526773f4bb10eadb835ab46ccf
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-11-11 01:11:56 +0900

    JPA,Repositoryを用いてDBアクセスするように修正

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,56 +1,74 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.repositories.MyDataRepository;
 // import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
+// @RestController JSON形式のデータを返す => Restfulなサービス作成
 @Controller
 public class HelloController {
-	String[] names = {"tanaka","sato","kato","suzuki","simizu"};
-	String[] emails = {"tanaka@email.com","sato@email.com","kato@email.com","suzuki@email.com","simizu@email.com"};
+	/*
+	 * 1, @Repository がつけられたインターフェースを検索し、自動的にそのクラスとインスタンスが作成され、インスタンスがBeanとしてアプリに登録される
+	 * 2, Controller クラスがロードされる際に@Autowiredが指定されているフィールドが存在する時、登録済みのBeanから同じクラスのものを検索し、自動的に
+	 * 　　そのBeanのインスタンスをフィールドに割り当てる
+	 */
+	@Autowired // 自動的にインスタンスが生成 => MyDataRepositoryってインターフェースだよ？ => Spring MVC により無名クラスのインスタンスが作成
+	MyDataRepository repository; // => MyDataRepository(インターフェース) => 無名クラス => インスタンス => Bean => Beanのインスタンス
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String index(Model model) {
-		model.addAttribute("msg", "フォームを送信してください");
-		return "index";
+	public ModelAndView index(ModelAndView mav) {
+		mav.setViewName("index");
+		mav.addObject("msg", "サンプル");
+		Iterable<MyData> list = repository.findAll();
+		mav.addObject("data", list);
+		return mav;
 	}
-	@RequestMapping(value="/", method=RequestMethod.POST)
-	public String indexPost(
-			Model model,
-			@RequestParam("fullName")String fullName,
-			@RequestParam("adress")String adress,
-			@RequestParam("memo")String memo,
-			@RequestParam(value="sex",required=false)String sex,
-			@RequestParam(value="os",required=false)String os,
-			@RequestParam(value="apply",required=false)boolean isApply
-			) {
-		model.addAttribute("postedMsg", "送信内容");
-		model.addAttribute("postedFullName", fullName);
-		model.addAttribute("postedAdress", adress);
-		model.addAttribute("postedSex", sex);
-		model.addAttribute("postedOs", os);
-		model.addAttribute("postedIsApply", isApply);
-		model.addAttribute("postedMemo", memo);
-		return "index";
-	}
-	@RequestMapping(value="/{num}")
-	public String indexWithParam(@PathVariable int num) {
-		int res = 0;
-		for(int i = 0; i <= num; i++) {
-			res += i;
-		}
-		String strRes = String.valueOf(res);
-		return "合計は" + strRes;
-	}
-	@RequestMapping(value="/id/{id}")
-	public obj getId(@PathVariable int id) {
-		return new obj(id, names[id], emails[id]);
-	}
+
+//	String[] names = {"tanaka","sato","kato","suzuki","simizu"};
+//	String[] emails = {"tanaka@email.com","sato@email.com","kato@email.com","suzuki@email.com","simizu@email.com"};
+
+//	@RequestMapping(value="/", method=RequestMethod.POST)
+//	public String indexPost(
+//			Model model,
+//			@RequestParam("fullName")String fullName,
+//			@RequestParam("adress")String adress,
+//			@RequestParam("memo")String memo,
+//			@RequestParam(value="sex",required=false)String sex,
+//			@RequestParam(value="os",required=false)String os,
+//			@RequestParam(value="apply",required=false)boolean isApply
+//			) {
+//		model.addAttribute("postedMsg", "送信内容");
+//		model.addAttribute("postedFullName", fullName);
+//		model.addAttribute("postedAdress", adress);
+//		model.addAttribute("postedSex", sex);
+//		model.addAttribute("postedOs", os);
+//		model.addAttribute("postedIsApply", isApply);
+//		model.addAttribute("postedMemo", memo);
+//		return "index";
+//	}
+
+//	@RequestMapping(value="/{num}")
+//	public String indexWithParam(@PathVariable int num) {
+//		int res = 0;
+//		for(int i = 0; i <= num; i++) {
+//			res += i;
+//		}
+//		String strRes = String.valueOf(res);
+//		return "合計は" + strRes;
+//	}
+
+//	@RequestMapping(value="/id/{id}")
+//	public obj getId(@PathVariable int id) {
+//		return new obj(id, names[id], emails[id]);
+//	}
 
 	class obj {
 		private int id;
